2968086295aaa1af60c2c5ccaeda13736d22bee8
gautam, prasanna <prasannagautam@gmail.com>
2014-09-27 14:56:30 -0400

    added the com.Splunk.Command directly in the code and using Splunk repos

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "https://github.com/elfsternberg/splunk-search"
   :license {:name "Apache Public Licence 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :repositories [["splunk-release" "http://splunk.artifactoryonline.com/splunk/ext-releases-local"]]
   :dependencies [
-                 [org.clojure/clojure "1.4.0"] 
-                 [commons-cli "1.2"] 
-                 [gson "2.1"] 
-                 [opencsv "2.3"]
-                 [splunk "1.1"]]
-  :plugins [[lein-localrepo "0.4.1"]]
+                 [org.clojure/clojure "1.5.0"]
+                 [commons-cli/commons-cli "1.2"]
+                 [com.google.code.gson/gson "2.1"]
+                 [net.sf.opencsv/opencsv "2.3"]
+                 [com.splunk/splunk "1.3.1.0"]]
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :main splunksearch.core)
-  
